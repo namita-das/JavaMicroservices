6e507cd113256f72b95fd2c5b993a64df3d6583d
namita <namita.das@ibm.com>
2022-05-02 17:51:06 +0530

    crO

--- src/main/java/org/ibm/springboot/controller/EmployeeController.java
+++ src/main/java/org/ibm/springboot/controller/EmployeeController.java
@@ -23,7 +23,7 @@ import com.cloudant.client.api.CloudantClient;
 import com.cloudant.client.api.Database;
 import com.cloudant.client.api.model.Response;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://169.51.204.190:31851")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
